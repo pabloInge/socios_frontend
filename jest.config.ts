14b23c24c5fccc54b7db237a6b56a76bca7c1196
Pablo Ingegnieri <pabloingegnieri1@gmail.com>
2026-04-10 21:24:09 -0300

    Cambios en la configuracion de test

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,8 @@ const createJestConfig = nextJest({
 
 const config: Config = {
     coverageProvider: 'v8',
-    testEnvironment: 'jsdom',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
 export default createJestConfig(config)
